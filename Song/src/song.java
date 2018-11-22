24727c4fd075f52dab90e3b8b033dbab5c21415f
psungchan <psungchan@yahoo.com>
2018-11-21 23:45:06 -0800

    started project 1

---
+++ Song/src/song.java
@@ -0,0 +1,68 @@
+// Sungchan Park
+// 11/20/2018
+// N/A
+// TA: N/A
+// Assignment #1
+//
+// This program will print out a song with several repeating lines through the use of several static
+// 	method calls.
+
+public class song {
+	public static void main(String[] args) {
+		
+		
+	}
+	public void firstStanza() {
+		System.out.println("");
+		System.out.println("");
+	}
+	
+	public void secondStanza() {
+		System.out.println("");
+		System.out.println("");
+	}
+	
+	public void thirdStanza() {
+		System.out.println("");
+		System.out.println("");
+	}
+	
+	public void fourthStanza() {
+		System.out.println("");
+		System.out.println("");
+	}
+	
+	public void fifthStanza() {
+		System.out.println("");
+		System.out.println("");
+	}
+	
+	public void myStanza() {
+		System.out.println("This song is bad.");
+		System.out.println("I am sad.");
+	}
+	
+	public void finalStanza() {
+		System.out.println("There was an old woman who swallowed a horse,");
+		System.out.println("She died of course.");
+	}
+	
+	public void firstLine() {
+		System.out.println ("There was and old woman who swallowed a fly.");
+	}
+	
+	public void lastLine() {
+		System.out.println ("Perhaps she'll die.");
+		System.out.println();
+	}
+	
+	public void secondLine() {
+		System.out.println("What a hog to swallow a dog.");
+		thirdLine();
+	}
+	
+	public void thirdLine() {
+		System.out.println("What a hog to swallow a dog.");
+	}
+	
+}
